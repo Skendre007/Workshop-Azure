d76a028efdef82b4522726b54576f78a8a6fe70c
Saurabh Kendre <154798561+Skendre007@users.noreply.github.com>
2024-02-02 16:47:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-/**
- * Application component
- *
- * To contain application wide settings, routes, state, etc.
- */
-
 import React from "react";
-
 import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
-
 import "./styles.css";
 
-/**
- * This object represents your information. The project is set so that you
- * only need to update these here, and values are passed a properties to the
- * components that need that information.
- *
- * Update the values below with your information.
- *
- * If you don't have one of the social sites listed, leave it as an empty string.
- */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Kendre Saurabh ",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  email: "saurabhkendre01@gmail.com",
+  gitHub: "Skendre007",
+  instagram: "Saurabh_kendre007",
   linkedIn: "satyanadella",
   medium: "",
   twitter: "microsoft",
